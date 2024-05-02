f33dc8b56c7ad6116fbdbcf35b88b6e96b7654d5
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-02 18:23:59 +0000

    afdad

--- app.py
+++ app.py
@@ -3,14 +3,8 @@ import pandas as pd
 
 st.title("Hello World")
 
-
-
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    #st.write("filename:", uploaded_file.name)
-    #st.write(bytes_data)
-
-
-dataframe = pd.read_csv(bytes_data)
-st.dataframe(dataframe)
+    dataframe = pd.read_csv(uploaded_file)
+    st.dataframe(dataframe)
