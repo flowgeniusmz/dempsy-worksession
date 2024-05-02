abaae869dcdb678a8d4d16a7dd5904ead86a673d
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-02 18:30:00 +0000

    adf

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import streamlit as st
 import pandas as pd
+from simple_salesforce import Salesforce
 
 st.title("Hello World")
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    dataframe = pd.read_csv(uploaded_file)
-    st.dataframe(dataframe)
+sfUsername = 'michael.zozulia@almalasers.com'
+sfPassword = 'EverlyQuinn#7665'
+sfToken = '8VY2ZFvXya0eyI1oGCMOZeVc'
+
+sf = Salesforce(username=sfUsername, password=sfPassword, security_token=sfToken)
+
+query = "SELECT Id, Name, Owner.Name FROM Account WHERE CreatedDate = LAST_N_DAYS:30"
+
+data = sf.query(query=query)
+records = data['records']
+rows = []
+for record in records:
+    newrow = {
+        'id' : record['Id'],
+        'name' : record['Name'],
+        'owner' : record['Owner']['Name']
+        }
+    rows.append(newrow)
+
+df = pd.DataFrame(rows)
+print(df)
+
+st.dataframe(data=df)
