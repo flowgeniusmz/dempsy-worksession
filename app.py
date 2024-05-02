7d03366c2f9ebdfa14967c484cb7c841cb3aabb1
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-02 18:21:54 +0000

    afda

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 
 st.title("Hello World")
 
@@ -8,5 +8,9 @@ st.title("Hello World")
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    #st.write("filename:", uploaded_file.name)
+    #st.write(bytes_data)
+
+
+dataframe = pd.DataFrame(bytes_data)
+st.dataframe(dataframe)
